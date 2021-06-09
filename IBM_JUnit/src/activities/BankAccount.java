fe8c53bae9bf7bb8539efd16d6e57654cb3a5add
Radha006 <85477013+Radha006@users.noreply.github.com>
2021-06-09 14:41:28 +0530

    Add files via upload

---
+++ IBM_JUnit/src/activities/BankAccount.java
@@ -0,0 +1,14 @@
+package activities;
+
+public class BankAccount {
+
+	public BankAccount(int i) {
+		// TODO Auto-generated constructor stub
+	}
+
+	public Object withdraw(int i) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
